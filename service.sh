d937635eaa8766ff08e7718fe27404fdff5d5d11
shadow3aaa <shadow3aaa@qq.com>
2024-01-22 12:26:01 +0800

    强制ipv4请求以解决ipv6无法劫持到smartdns

--- service.sh
+++ service.sh
@@ -3,7 +3,14 @@ MODDIR=${0%/*}
 $MODDIR/smartdns -c /data/smartdns.conf -p -
 
 settings put global private_dns_mode off
-iptables -t nat -D OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:53
-iptables -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53
-iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:53
-iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53
+iptables -t nat -N smartdns
+iptables -t nat -F smartdns
+iptables -t nat -A smartdns -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53
+iptables -t nat -A smartdns -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:53
+
+ip6tables -t filter -D OUTPUT -p tcp --dport 53 -j DROP
+ip6tables -t filter -D OUTPUT -p udp --dport 53 -j DROP
+ip6tables -t filter -D OUTPUT -p tcp --dport 53 -j REJECT
+ip6tables -t filter -D OUTPUT -p udp --dport 53 -j REJECT
+ip6tables -t filter -A OUTPUT -p tcp --dport 53 -j REJECT
+ip6tables -t filter -A OUTPUT -p udp --dport 53 -j REJECT
