c4327e3a32f08b96e6d1a058be834c4c51cac45a
shadow3aaa <shadow3aaa@qq.com>
2024-01-22 16:15:05 +0800

    修复不生效

--- service.sh
+++ service.sh
@@ -1,12 +1,14 @@
 MODDIR=${0%/*}
 
+killall smartdns
+sleep 1s
 $MODDIR/smartdns -c /data/smartdns.conf -p -
 
 settings put global private_dns_mode off
-iptables -t nat -N smartdns
-iptables -t nat -F smartdns
-iptables -t nat -A smartdns -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53
-iptables -t nat -A smartdns -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:53
+iptables -t nat -D OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:53 || true
+iptables -t nat -D OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53 || true
+iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination 127.0.0.1:53
+iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination 127.0.0.1:53
 
 ip6tables -t filter -D OUTPUT -p tcp --dport 53 -j DROP
 ip6tables -t filter -D OUTPUT -p udp --dport 53 -j DROP
